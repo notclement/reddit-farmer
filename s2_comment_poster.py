2fafa485774a005805b53444eaf0a39989d3d1b3
Clement <clement_392@hotmail.com>
2021-03-30 15:50:20 +0800

    Update s2_comment_poster.py

--- s2_comment_poster.py
+++ s2_comment_poster.py
@@ -4,42 +4,27 @@
 s2_comment_poster.py will post a random comment taken from the bulk comments from
 the previous day
 """
-
-from custom_comments import custom_comments
-from s1_comment_scraper import *
-from datetime import datetime, timedelta
-
-
-def read_from_file_into_lst(filepath):
-    lst = []
-    with open(filepath) as fp:
-        for line in fp:
-            lst.append(line.strip())
-    return lst
+import os
+from functions import *
 
 
 def main():
-    yesterday = datetime.now() - timedelta(1)
-    yesterday = yesterday.strftime('%d-%m-%Y-comments.txt')
-    filepath_yesterday = PATH + yesterday
-    lst_comment = read_from_file_into_lst(filepath_yesterday)
+    if len(sys.argv) != 2:
+        print('Program ended, need to give 1 arg to specify account to use')
+        sys.exit(0)
 
-    # ========= get 1 rando comment ===========
-    # if not 0, get from post comments
-    if random.randint(0, 2):
-        print('============ copied comments ============')
-        comment = get_random_comment(lst_comment)
-    # if 0, will get from custom comments
-    else:
-        print('============ custom comments ============')
-        comment = get_random_comment(custom_comments)
+    user = sys.argv[1]
 
-    print(comment)
-    print('=========================================')
-    # ==========================================
+    daily_obj = get_daily_obj(user)
 
-    # # post live, don't spam this mate
-    get_daily_obj().reply(replace_words(comment).capitalize())
+    # This is mostly to check for first run, to create the common used file
+    used_path = USED_PATH
+    if not os.path.isfile(used_path):
+        f = open(used_path, "w")
+        f.write('')
+        f.close()
+
+    post_rand_comment(daily_obj)
 
 
 if __name__ == '__main__':
